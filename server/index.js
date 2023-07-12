e762f835eeeaadf401ce5c78139033c1fe1a59be
AnshumanDec2K <dalai.anshuman2000@gmail.com>
2023-07-12 17:34:37 +0530

    basic mern setup done

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from "./routes/post.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://Ansh_123:Ansh%402023@cluster0.f6qsddh.mongodb.net/?retryWrites=true&w=majority";
