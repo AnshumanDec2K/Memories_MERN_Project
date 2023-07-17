0357d5a92685ee334d7f2d44fdffb07fc1e190bd
AnshumanDec2K <dalai.anshuman2000@gmail.com>
2023-07-17 20:16:25 +0530

    Pagination and search UI

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,24 +1,17 @@
-import express from "express";
+import express from 'express';
 
-import {
-  getPosts,
-  // getPost,
-  createPost,
-  updatePost,
-  likePost,
-  deletePost,
-} from "../controllers/post.js";
-
-import auth from "../middleware/auth.js";
+import { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/post.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
-//use controllers for getting callbacks
-router.get("/", getPosts);
-router.post("/", auth, createPost);
-// router.get("/:id", getPost);
-router.patch("/:id", auth, updatePost);
-router.delete("/:id", auth, deletePost);
-router.patch("/:id/likePost", auth, likePost);
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
